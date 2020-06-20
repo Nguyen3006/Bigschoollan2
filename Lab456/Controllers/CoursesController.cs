967cbd244dbfb755d8eb74abcb2b85404eabb0ba
Admin <Admin@DESKTOP-FQIT40P>
2020-06-20 19:17:46 +0700

    Add project files.

---
+++ Lab456/Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using Lab456.Models;
+using Lab456.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbConText;
+        public CoursesController()
+        {
+            _dbConText = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbConText.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbConText.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbConText.Courses.Add(course);
+            _dbConText.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Lab456/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Lab456.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab456.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcommingCourses);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
